b3f470c50aff50251c5daa41a3621ecacfb788bd
Kseniia <kore-ksenia@yandex.ru>
2025-08-13 00:36:22 +0300

    codestyle

--- stats/stats-server/src/main/java/ru/practicum/repository/StatsRepository.java
+++ stats/stats-server/src/main/java/ru/practicum/repository/StatsRepository.java
@@ -12,28 +12,28 @@ import java.util.List;
 public interface StatsRepository extends JpaRepository<Hit, Long> {
 
     @Query("SELECT new ru.practicum.ViewStatsDto(h.app, h.uri, COUNT(h.ip)) " +
-            "FROM EndpointHit h " +
+            "FROM Hit h " +
             "WHERE h.timestamp BETWEEN :start AND :end " +
             "GROUP BY h.app, h.uri " +
             "ORDER BY COUNT(h.ip) DESC")
     List<ViewStatsDto> getStatsAll(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end);
 
     @Query("SELECT new ru.practicum.ViewStatsDto(h.app, h.uri, COUNT(h.ip)) " +
-            "FROM EndpointHit h " +
+            "FROM Hit h " +
             "WHERE h.timestamp BETWEEN :start AND :end AND h.uri IN :uris " +
             "GROUP BY h.app, h.uri " +
             "ORDER BY COUNT(h.ip) DESC")
     List<ViewStatsDto> getStatsAllForUris(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end, @Param("uris") List<String> uris);
 
     @Query("SELECT new ru.practicum.ViewStatsDto(h.app, h.uri, COUNT(DISTINCT h.ip)) " +
-            "FROM EndpointHit h " +
+            "FROM Hit h " +
             "WHERE h.timestamp BETWEEN :start AND :end " +
             "GROUP BY h.app, h.uri " +
             "ORDER BY COUNT(DISTINCT h.ip) DESC")
     List<ViewStatsDto> getStatsUniqueIp(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end);
 
     @Query("SELECT new ru.practicum.ViewStatsDto(h.app, h.uri, COUNT(DISTINCT h.ip)) " +
-            "FROM EndpointHit h " +
+            "FROM Hit h " +
             "WHERE h.timestamp BETWEEN :start AND :end AND h.uri IN :uris " +
             "GROUP BY h.app, h.uri " +
             "ORDER BY COUNT(DISTINCT h.ip) DESC")
